9072e2a511d527675513ddf40d8a464f91887a6d
Aneta Biederman <aneta.biedermanova@seznam.cz>
2018-09-13 12:39:42 +0200

    create fragments, viewPagerAdapter

--- app/src/main/java/com/example/android/news/MainActivity.java
+++ app/src/main/java/com/example/android/news/MainActivity.java
@@ -2,27 +2,28 @@ package com.example.android.news;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
+
 import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
 
 import com.example.android.news.adapter.NewsAdapter;
+import com.example.android.news.adapter.ViewPagerAdapter;
+import com.example.android.news.fragment.FragmentABC;
+import com.example.android.news.fragment.FragmentCNN;
 import com.example.android.news.model.News;
-import com.example.android.news.model.NewsList;
-import com.example.android.news.network.GetDataService;
-import com.example.android.news.network.RetrofitClientInstance;
 
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
 
     private List<News> newsList;
     private NewsAdapter adapter;
@@ -36,47 +37,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         context = getApplicationContext();
 
-        progressDialog = new ProgressDialog(MainActivity.this);
-        progressDialog.setMessage("Loading Data.. Please wait...");
-        progressDialog.setIndeterminate(false);
-        progressDialog.setCancelable(false);
-        progressDialog.show();
+        tabLayout = findViewById(R.id.tablayout_id);
+        viewPager = findViewById(R.id.viewpage_id);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-        //Creating an object of our api interface
-        GetDataService api = RetrofitClientInstance.getDataService();
+        viewPagerAdapter.addFragment(new FragmentCNN(), "CCN news");
+        viewPagerAdapter.addFragment(new FragmentABC(), "ABC news");
 
-         //Calling JSON
-        Call<NewsList> call = api.getAllCNNNews();
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
-        /**
-         * Enqueue Callback will be call when get response...
-         */
-        call.enqueue(new Callback<NewsList>() {
-            @Override
-            public void onResponse(Call<NewsList> call, Response<NewsList> response) {
-                //Dismiss Dialog
-                progressDialog.dismiss();
-                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
-
-                if (response.isSuccessful()) {
-                    /**
-                     * Got Successfully
-                     */
-                    newsList = response.body().getArticles();
-                    recyclerView = findViewById(R.id.customRecyclerView);
-                    adapter = new NewsAdapter(context, newsList);
-                    RecyclerView.LayoutManager eLayoutManager = new LinearLayoutManager(getApplicationContext());
-                    recyclerView.setLayoutManager(eLayoutManager);
-                    recyclerView.setItemAnimator(new DefaultItemAnimator());
-                    recyclerView.setAdapter(adapter);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<NewsList> call, Throwable t) {
-                progressDialog.dismiss();
-
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/example/android/news/fragment/FragmentABC.java
@@ -0,0 +1,93 @@
+package com.example.android.news.fragment;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.news.R;
+import com.example.android.news.adapter.NewsAdapter;
+import com.example.android.news.model.News;
+import com.example.android.news.model.NewsList;
+import com.example.android.news.network.GetDataService;
+import com.example.android.news.network.RetrofitClientInstance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class FragmentABC extends Fragment {
+
+    View v;
+    private List<News> newsList;
+    private NewsAdapter adapter;
+    private RecyclerView recyclerView;
+    ProgressDialog progressDialog;
+    Context context;
+
+    public FragmentABC () {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment, container, false);
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getActivity();
+
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Loading Data.. Please wait...");
+        progressDialog.setIndeterminate(false);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        //Creating an object of our api interface
+        GetDataService api = RetrofitClientInstance.getDataService();
+
+        //Calling JSON
+        Call<NewsList> call = api.getAllABCNews();
+
+        /**
+         * Enqueue Callback will be call when get response...
+         */
+        call.enqueue(new Callback<NewsList>() {
+            @Override
+            public void onResponse(Call<NewsList> call, Response<NewsList> response) {
+                //Dismiss Dialog
+                progressDialog.dismiss();
+
+                if (response.isSuccessful()) {
+                    /**
+                     * Got Successfully
+                     */
+                    newsList = response.body().getArticles();
+                    recyclerView = v.findViewById(R.id.customRecyclerView);
+                    adapter = new NewsAdapter(context, newsList);
+                    RecyclerView.LayoutManager eLayoutManager = new LinearLayoutManager(getActivity());
+                    recyclerView.setLayoutManager(eLayoutManager);
+                    recyclerView.setItemAnimator(new DefaultItemAnimator());
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<NewsList> call, Throwable t) {
+                progressDialog.dismiss();
+
+            }
+        });}
+}
+
